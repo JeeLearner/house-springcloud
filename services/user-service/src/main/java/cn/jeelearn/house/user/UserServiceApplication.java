42966cba5bb9d611ec064283a52d4ca76ccc6fad
jeelearner <jeelearner@163.com>
2018-12-20 17:14:37 +0800

    init

---
+++ services/user-service/src/main/java/cn/jeelearn/house/user/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package cn.jeelearn.house.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
+
